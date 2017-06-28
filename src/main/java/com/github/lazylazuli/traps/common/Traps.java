b774fdb7e419407370fda34a4e1e9046ca1dfd5e
Rain Ang <rainnier.ang@gmail.com>
2017-06-29 02:26:05 +0800

    Reformat

--- src/main/java/com/github/lazylazuli/traps/common/Traps.java
+++ src/main/java/com/github/lazylazuli/traps/common/Traps.java
@@ -8,22 +8,33 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Traps.MODID, version = Traps.VERSION + "-" + Traps.BUILD, acceptedMinecraftVersions = Traps.MCVERSION,
-		dependencies = "required-after:lazylazulilib@[2.0.0]", updateJSON = Traps.UPDATE)
+@Mod(modid = Traps.MODID,
+	 version = Traps.VERSION + "-" + Traps.BUILD,
+	 acceptedMinecraftVersions = Traps.MCVERSION,
+	 dependencies = "required-after:lazylazulilib@[2.0.0]",
+	 updateJSON = Traps.UPDATE)
 public class Traps extends LazyLazuliMod
 {
 	public static final String MODID = "lazylazulitraps";
+	
+	public static final String MCVERSION = "1.12";
+	
 	public static final String VERSION = "2.0.0";
 	public static final String BUILD = "41";
-	public static final String MCVERSION = "1.12";
+	
 	public static final String UPDATE = "https://github.com/lazylazuli/Traps/releases/download/Latest/update.json";
 	
 	@Mod.Instance
 	public static Traps instance;
 	
-	@SidedProxy(clientSide = "com.github.lazylazuli.traps.client.ClientProxy", serverSide = "com.github.lazylazuli" +
-			".traps.common.CommonProxy")
+	@SidedProxy(clientSide = "com.github.lazylazuli.traps.client.ClientProxy",
+				serverSide = "com.github.lazylazuli.traps.common.CommonProxy")
 	public static Proxy proxy;
+	
+	public Traps()
+	{
+		instance = this;
+	}
 	
 	@Override
 	public String getId()
