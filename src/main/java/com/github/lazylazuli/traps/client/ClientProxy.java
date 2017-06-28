f8e1e5727c5ea9f47969105ea76ec611dcb4fd22
Rain Ang <rainnier.ang@gmail.com>
2017-06-29 02:49:55 +0800

    Fix model resource registry

--- src/main/java/com/github/lazylazuli/traps/client/ClientProxy.java
+++ src/main/java/com/github/lazylazuli/traps/client/ClientProxy.java
@@ -1,18 +1,17 @@
 package com.github.lazylazuli.traps.client;
 
 import com.github.lazylazuli.traps.common.CommonProxy;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import static com.github.lazylazuli.traps.common.TrapObjects.*;
 
 public class ClientProxy extends CommonProxy
 {
-	@Override
-	public void preInit(FMLPreInitializationEvent event)
+	@SubscribeEvent
+	public void registerModels(ModelRegistryEvent event)
 	{
-		super.preInit(event);
-		
 		setModelResourceFor(GRASS_TRAP);
 		
 		setModelResourceFor(
